f7e0bcb29d82ff5a6c9363c52b6c1dd9364dc3ef
tedgiggles <41264910+tedgiggles@users.noreply.github.com>
2025-03-28 00:14:01 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 TELEGRAM_TOKEN = '7207668599:AAEwnxvirQz6qf2YGFG0PULCwgmU8HnzgDU'
 CHAT_ID = '5945413679'
 
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.form
@@ -14,10 +13,9 @@ def webhook():
     phone = data.get('your-phone', 'Без телефону')
 
     message = f'📥 Нова заявка з сайту:\n👤 Імʼя: {name}\n📞 Телефон: {phone}'
-
+    
     send_to_telegram(message)
     return 'OK', 200
-
 
 def send_to_telegram(text):
     url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
@@ -26,7 +24,6 @@ def send_to_telegram(text):
         'text': text
     }
     requests.post(url, data=payload)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
